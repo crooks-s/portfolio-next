76ef8787a182559d1d7b3853bfbca2062b87bc06
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-03-01 13:54:23 -0800

    update og portfolio link

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -50,7 +50,7 @@ const projectsData = [
     description: "Project 6 description",
     image: "/images/projects/6.png",
     gitUrl: "https://github.com/crooks-s/static_node_and_express_site-v1",
-    liveUrl: "https://crooks.up.railway.app/",
+    liveUrl: "crooksog.up.railway.app",
   },
   {
     id: 7,
